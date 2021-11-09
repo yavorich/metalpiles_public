103d2c130cea94e690d8108dfa5adfab1099d883
George Chaykin <56233756+globalpie271@users.noreply.github.com>
2021-11-09 23:26:25 +0300

    img extraction from json file added

--- save_yolo_format.py
+++ save_yolo_format.py
@@ -3,14 +3,30 @@ import os
 import glob
 import argparse
 import shutil
+import base64
 import json
 import cv2
+import numpy as np
 
 
 id_list = ['valid_metal', 'no_metal', 'long_metal', 'thick_metal',
            'grapple', 'truck', 'magnet']
 id_dict = {name: i for i, name in enumerate(id_list)}
 
+
+def b64_to_img(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+        img = data['imageData']
+        img = base64.b64decode(img)
+        img = np.frombuffer(img, dtype = np.uint8)
+        img = cv2.imdecode(img, 1)
+        return img
+
+# def save_img(img_path, img):
+    # cv2.imwrite(impg_path, image)
+    
+    
 def save_yolo_file(labels, video_name, frame_name,  save_dir):
     txt_path = os.path.join(frame_name + f'({video_name})' + '.txt')
     txt_path = os.path.join(save_dir, txt_path)
@@ -21,24 +37,50 @@ def save_yolo_file(labels, video_name, frame_name,  save_dir):
     else:
          print(f'{frame_name}.json doesn\'t have bounding boxes. Please, check this file.')
         
-def save_jpg_file(frame_path, video_name,  save_dir):
+# def save_jpg_file(frame_path, video_name,  save_dir):
+    # fn = os.path.basename(frame_path).split('.')[0] + f'({video_name})' + '.jpg' 
+    # fn = os.path.basename(frame_path).split('.')[0] + f'({video_name})' + '.json' 
+    # print(frame_path)
+    # new_path = os.path.join(save_dir, fn)
+    # shutil.copy(frame_path,  new_path)
+    # img = b64_to_img(frame_path)
+    # cv2.imwrite(new_path, img)
+
+def save_jpg_file(img, frame_path, video_name,  save_dir):
     fn = os.path.basename(frame_path).split('.')[0] + f'({video_name})' + '.jpg' 
+    # fn = os.path.basename(frame_path).split('.')[0] + f'({video_name})' + '.json' 
+    # print(frame_path)
     new_path = os.path.join(save_dir, fn)
-    shutil.copy(frame_path,  new_path)
+    # shutil.copy(frame_path,  new_path)
+    # img = b64_to_img(frame_path)
+    cv2.imwrite(new_path, img)
+
 
 
 def create_yolo_files(frames_path, video_name, save_dir):
     for json_path in glob.glob(os.path.join(frames_path, '*.json')):
         with open(json_path) as f:
-            data = json.load(f)
+            data_str = f.read()
+            if '{' in data_str:
+                data = json.loads(data_str)
+            else:
+                print(f'{json_path} is empty. Please, check this file.')
+            
         ports_list = []
+        
+        img = data['imageData']
+        img = base64.b64decode(img)
+        img = np.frombuffer(img, dtype = np.uint8)
+        img = cv2.imdecode(img, 1)
+        
         for i in range(len(data['shapes'])):
             ports_location = data['shapes'][i]['points']
             ports_list.append(ports_location)
         
         frame_path = json_path.split('.')[0] + '.jpg'
         frame_name = os.path.basename(json_path).split('.')[0]
-        save_jpg_file(frame_path, video_name, save_dir)
+        # save_jpg_file(frame_path, video_name, save_dir)
+        save_jpg_file(img, frame_path, video_name, save_dir)
         labels = []
         for i in range(len(data['shapes'])):
             x_lists=[]
@@ -55,7 +97,9 @@ def create_yolo_files(frames_path, video_name, save_dir):
             width = x_max-x_min
             height = y_max-y_min
             
-            u,v,_ = cv2.imread(frame_path).shape
+            # u,v,_ = cv2.imread(frame_path).shape
+            u,v,_ = img.shape
+            
                 
             center_x,center_y = round(float((x_min+width/2.0)/v),6),round(float((y_min+height/2.0)/u),6)
             
